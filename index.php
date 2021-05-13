2a87e03ae3fcf7aa4a79da07971f19934e83891f
Rafael Ramírez <takeshie01@gmail.com>
2021-05-13 08:18:49 -0700

    Connection
    Modified connection parameters

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php 
     require_once("classes/connection/connect.php");
     $conexion = new connect;
-
-    //$query = "INSERT INTO pacientes (DNI) VALUE('1')";
-    //print_r($conexion->nonQueryId($query));
 ?>
 
 hola index
