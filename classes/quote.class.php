51ef28efd443e2a5380a216206128fcbc81efee6
Rafael Ramírez <takeshie01@gmail.com>
2021-05-21 12:02:38 -0500

    Backbone
    Basic Backbone structure for the quote class

--- classes/quote.class.php
+++ classes/quote.class.php
@@ -1,9 +1,72 @@
 <?php 
     require_once("response.class.php");
-    //Prueba sexual llanera de la clase model
-    class quote extends cls_dbtools //Se omite temporalmente la clase Model ya que no representa una necesidad inmediata
-    {
+    //Ayyy lmao yo no había borrado esto
+    /*
 
+    METODOS MAS IMPORTANTES POR AGREGAR:
+    -Función AÑADIR ORDEN
+    -Función REPORTAR ORDEN
+    -Función REPORTAR ORDEN MASTER
+    -Función AÑADIR UPGRADE
+    -Función SOLICITUD DE ANULACIÓN
+    -Función SOLICITAR CANCELACION DE UPGRADE 
+    
+    CAMBIOS A ORDENES AGREGADAS/REPORTADAS:
+    -Función COMPROBAR PRE-ORDEN
+    -Función SOLICITAR CAMBIOS
+    -Función CAMBIOS EN ORDENES REPORTADAS
+
+    Adicionales:
+    -Función AGREGAR SUSCRIPCION --------------
+    -Función REPORTAR SUSCRIPCIÓN --------------
+    -Función CAMBIOS EN SUSCRIPCION --------------
+    -Función EXTENDER SUSBCRIPCION --------------
+    -Función CANCELAR SUSCRIPCION --------------
+
+    IMPORTANTE IMPLEMENTAR:
+    *trans_all_webservices: se registra todo el movimiento de la DB, principalmente los datos que introdujo el usuario
+    y la respuesta que dio el servidor
+    *log_consultas: registra todas las consultas que llegan a la DB, referentes a inserts, updates y deletes principalmente
+
+    */
+    class quote extends cls_dbtools //Se omite temporalmente la clase Model ya que no representa una necesidad inmediata
+    {   
+        public function put($data){
+            //Recibimos los datos, empieza el jogo bonito
+            $_respuesta = new response;
+            
+            $datos = json_decode($data,true);
+            $request = $datos['request'];
+            switch ($request) {
+                case 'add_order':
+                    # code...
+                    break;
+                
+                case 'report_order':
+                    # code...
+                    break;
+                
+                case 'report_order_master':
+                    # code...
+                    break;
+
+                case 'add_upgrade':
+                    # code...
+                    break;
+                
+                case 'request_cancellation':
+                    # code...
+                    break;
+
+                case 'request_upgrade_cancellation':
+                    # code...
+                    break;
+
+                default:
+                    # code...
+                    break;
+            }
+        }
         public function selectDynamic($filters, $table = string, $where = '1', $fields, $querys = '', $die = false, $limit = 6)
         {
             /**
@@ -32,6 +95,56 @@
             }
             //return parent::obtenerDatos($query);
             return $this->_SQL_tool($this->SELECT, __METHOD__, $query);
+        }
+
+        public function insertDynamic($data=Array(), $table=null){
+            if (empty($table) || count($data) == 0) {
+                return false;
+            }
+            $arrFiels       = [];
+            $arrValues      = [];
+            $SQL_functions  = [
+                'NOW()'
+            ];
+    
+            foreach ($data as $key => $value) {
+                $arrFiels[] = '`'. $key .'`';
+                if (in_array(strtoupper($value), $SQL_functions)) {
+                    $arrValues[] = strtoupper($value);
+                } else {
+                    $arrValues[] = '\''. $value .'\'';
+                }
+            }
+    
+            $query = "INSERT INTO $table (". implode(',', $arrFiels) .") VALUES (". implode(',', $arrValues) .")";
+            return $this->_SQL_tool($this->INSERT, __METHOD__,$query);  
+        }
+
+        public function updateDynamic($table,$field,$fieldwere,$data,$and){
+        
+            (!empty($table))    ?: $table;
+            (!empty($field))    ?: $field;
+            (!empty($fieldwere))?: $fieldwere;
+    
+            $query="UPDATE $table SET ";
+            $cadQuery='';
+            foreach ($data as $key => $value) {
+                if(!empty($value)){
+                    $cadQuery .= $key.'='."'".$value."'".',';
+                }
+            }
+            
+            $query .= mb_strrchr($cadQuery,',',true);
+            $query .= " WHERE $table.$field = '$fieldwere' ";
+            if(!empty($and)){
+                $andField=$and['field'];
+                $andValue=$and['value'];
+                $query .= " AND $andField = '$andValue' ";
+            }
+            if(!empty($cadQuery)){
+            $resp   = $this->_SQL_tool($this->UPDATE, __METHOD__, $query);
+            }
+            return ($query)?true:false;
         }
     }
     
